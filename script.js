fd4ca660ddf5a0724342786d1a618610dce0e08f
ideaspaper <agni.infinity@gmail.com>
2020-11-05 15:24:34 +0700

    refactor: split data from script

--- script.js
+++ script.js
@@ -1,94 +1,3 @@
-var data = [
-  {
-    clue: 'Other programmers from different programming language should be able to understand my code if I make a ___.',
-    answer: 'pseudocode',
-    x: 1,
-    y: 1,
-    orientation: 'across',
-    legend: 1
-  },
-  {
-    clue: 'I am one of the primitive data types in JavaScript.',
-    answer: 'number',
-    x: 7,
-    y: 3,
-    orientation: 'across',
-    legend: 4
-  },
-  {
-    clue: 'One of the conventions when making a variable name.',
-    answer: 'camelcase',
-    x: 12,
-    y: 6,
-    orientation: 'across',
-    legend: 5
-  },
-  {
-    clue: 'You should make an ___ before implementing your code.',
-    answer: 'algorithm',
-    x: 4,
-    y: 8,
-    orientation: 'across',
-    legend: 6
-  },
-  {
-    clue: 'Use ___ in pseudocode to print something.',
-    answer: 'display',
-    x: 3,
-    y: 11,
-    orientation: 'across',
-    legend: 8
-  },
-  {
-    clue: 'The creator of JavaScript.',
-    answer: 'brendaneich',
-    x: 11,
-    y: 11,
-    orientation: 'across',
-    legend: 9
-  },
-  {
-    clue: 'true false',
-    answer: 'boolean',
-    x: 6,
-    y: 15,
-    orientation: 'across',
-    legend: 10
-  },
-  {
-    clue: 'IF in pseudocode should be ended with ___.',
-    answer: 'endif',
-    x: 3,
-    y: 1,
-    orientation: 'down',
-    legend: 2
-  },
-  {
-    clue: 'if is syntax for making a ___ statement.',
-    answer: 'conditional',
-    x: 7,
-    y: 1,
-    orientation: 'down',
-    legend: 3
-  },
-  {
-    clue: '==, !==, ===, !==, >, <, >=, <=.',
-    answer: 'comparison',
-    x: 12,
-    y: 6,
-    orientation: 'down',
-    legend: 5
-  },
-  {
-    clue: 'I am one of the primitive data types in JavaScript.',
-    answer: 'string',
-    x: 19,
-    y: 8,
-    orientation: 'down',
-    legend: 7
-  },
-];
-
 function createTable(questions) {
   const maxRowCol = [0, 0];
 
@@ -195,9 +104,10 @@ function isRight(questions) {
   }
 }
 
-function gotoIndex(x, y) {
-  console.log(x, y);
-}
+let title = document.getElementsByTagName('title');
+title[0].innerHTML = crosswordsTitle;
+let heading = document.getElementsByTagName('h1');
+heading[0].innerHTML = crosswordsTitle;
 
 let crosswordsContainer = document.getElementById('crosswords-container');
 let crosswordsClues = document.getElementById('crosswords-clues');
