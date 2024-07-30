d24ec4ab6af5bfed01089f75ede5e90c27caa99d
Daniel Kristianto Haryono <daniel.haryono@shopee.com>
2024-07-30 09:25:44 +0700

    feat: update questions for QAE

--- data.js
+++ data.js
@@ -1,91 +1,67 @@
-const crosswordsTitle = 'Crosswords - eps. 1';
+const crosswordsTitle = 'QAE: Testing vs Debugging and Testing Principles';
 const data = [
   {
-    clue: 'Other programmers from different programming language should be able to understand my code if I make a ___.',
-    answer: 'pseudocode',
-    x: 1,
+    clue: '___ paradox: The same set of test cases, when repeated over time, will no longer find new defects',
+    answer: 'Pesticide',
+    orientation: 'down',
+    x: 7,
     y: 1,
-    orientation: 'across',
-    legend: 1
+    legend: 1,
   },
   {
-    clue: 'I am one of the primitive data types in JavaScript.',
-    answer: 'number',
-    x: 7,
+    clue: `Test results return no errors; therefore, it should fulfill the client's needs`,
+    answer: 'misconception',
+    orientation: 'across',
+    x: 5,
     y: 3,
-    orientation: 'across',
-    legend: 4
+    legend: 2,
   },
   {
-    clue: 'One of the conventions when making a variable name.',
-    answer: 'camelcase',
-    x: 12,
+    clue: 'Debugging should be done ___',
+    answer: 'manually',
+    orientation: 'down',
+    x: 17,
+    y: 1,
+    legend: 3,
+  },
+  {
+    clue: 'Debugging is for ___ defects',
+    answer: 'removing',
+    orientation: 'down',
+    x: 10,
     y: 6,
-    orientation: 'across',
-    legend: 5
+    legend: 4,
   },
   {
-    clue: 'You should make an ___ before implementing your code.',
-    answer: 'algorithm',
-    x: 4,
-    y: 8,
+    clue: 'Exhaustive testing is ___',
+    answer: 'impossible',
     orientation: 'across',
-    legend: 6
+    x: 1,
+    y: 7,
+    legend: 5,
   },
   {
-    clue: 'Use ___ in pseudocode to print something.',
-    answer: 'display',
-    x: 3,
-    y: 11,
+    clue: 'The first step in the debugging process is ___ the bug',
+    answer: 'reproducing',
     orientation: 'across',
-    legend: 8
-  },
-  {
-    clue: 'The creator of JavaScript.',
-    answer: 'brendaneich',
-    x: 11,
-    y: 11,
-    orientation: 'across',
-    legend: 9
-  },
-  {
-    clue: 'true false',
-    answer: 'boolean',
     x: 6,
-    y: 15,
+    y: 9,
+    legend: 6,
+  },
+  {
+    clue: 'As much as possible, testing should be done ___',
+    answer: 'automatically',
     orientation: 'across',
-    legend: 10
-  },
-  {
-    clue: 'IF in pseudocode should be ended with ___.',
-    answer: 'endif',
     x: 3,
-    y: 1,
-    orientation: 'down',
-    legend: 2
+    y: 11,
+    legend: 7,
   },
   {
-    clue: 'if is syntax for making a ___ statement.',
-    answer: 'conditional',
-    x: 7,
-    y: 1,
-    orientation: 'down',
-    legend: 3
-  },
-  {
-    clue: '==, !==, ===, !==, >, <, >=, <=.',
-    answer: 'comparison',
-    x: 12,
-    y: 6,
-    orientation: 'down',
-    legend: 5
-  },
-  {
-    clue: 'I am one of the primitive data types in JavaScript.',
-    answer: 'string',
-    x: 19,
-    y: 8,
-    orientation: 'down',
-    legend: 7
+    clue: 'Testing is for ___ defects in the test object',
+    answer: 'finding',
+    orientation: 'across',
+    x: 4,
+    y: 13,
+    legend: 8,
   },
 ];
